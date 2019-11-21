2d7e549208dc46fdaade38a1f17d455ed8413379
OlgaIlyutko <39804311+OlgaIlyutko@users.noreply.github.com>
2019-11-21 10:57:04 +0300

    Исправления Трэвис

--- src/main.js
+++ src/main.js
@@ -2,17 +2,17 @@
 
 const createUserRankTemplate = () =>
     `<section class="header__profile profile">
-        <p class="profile__rating">Movie Buff</p>
-        <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+      <p class="profile__rating">Movie Buff</p>
+      <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
     </section>`;
 
 const createMenuTemplate = () =>
 	`<nav class="main-navigation">
-        <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
-        <a href="#stats" class="main-navigation__item main-navigation__item--additional">Stats</a>
+    <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+    <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
+    <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
+    <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+    <a href="#stats" class="main-navigation__item main-navigation__item--additional">Stats</a>
 	</nav>`;
 
 const createSortTemplate = () =>
