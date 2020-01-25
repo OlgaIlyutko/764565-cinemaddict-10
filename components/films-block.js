bb62df18c6ca2bf52c413cab56bfe40c3f94a10a
OlgaIlyutko <39804311+OlgaIlyutko@users.noreply.github.com>
2020-01-25 10:16:59 +0300

    Delete films-block.js

--- components/films-block.js
+++
@@ -1,9 +0,0 @@
-import AbstractComponent from './abstract-component';
-
-const createFilmsBlockTemplate = () => `<section class="films"></section>`;
-
-export default class FilmsBlock extends AbstractComponent {
-  getTemplate() {
-    return createFilmsBlockTemplate();
-  }
-}
